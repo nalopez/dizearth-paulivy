f75acb5ae726998b26215e41676732493890bea1
Nicole Anne Lopez <lopez.nicoleanne@gmail.com>
2020-05-21 09:03:17 +0800

    Initial commit

---
+++ src/components/banner-title.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BannerTitle = () => {
+  return <h1> DizEarth-Paulivy </h1>;
+};
+
+export default BannerTitle;
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import BannerTitle from "../components/banner-title";
+import Menu from "../components/menu";
+import Footer from "../components/footer";
+
+function About() {
+  return (
+    <div className="main">
+      <BannerTitle />
+      <Menu />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default About;
